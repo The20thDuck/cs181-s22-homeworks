8e028f636d19eb3472e606f78223527a280aa13c
Warren Sunada-Wong <warren.sunada.wong@gmail.com>
2022-02-10 12:22:18 -0500

    hw1 p1

--- HW1/T1_P1.py
+++ HW1/T1_P1.py
@@ -5,6 +5,10 @@
 ##################
 
 import numpy as np
+import matplotlib
+matplotlib.use('WebAgg')
+import matplotlib.pyplot as plt
+
 
 data = [(0., 0.),
         (1., 0.5),
@@ -14,10 +18,36 @@ data = [(0., 0.),
         (6., 1.5),
         (8., 0.5)]
 
+def f(x, tau, data=data):
+    y = 0
+    for x0, y0 in data:
+        if x0 != x:
+            k = np.exp(-(x-x0)**2/tau)
+            y += k*y0
+    return y
+
 def compute_loss(tau):
     # TODO
     loss = 0
+    for x, y in data:
+        loss += (f(x, tau) - y)**2
     return loss
 
-for tau in (0.01, 2, 100):
-    print("Loss for tau = " + str(tau) + ": " + str(compute_loss(tau)))
+
+def f_plot(x, tau, data=data):
+    y = 0
+    for x0, y0 in data:
+        k = np.exp(-(x-x0)**2/tau)
+        y += k*y0
+    return y
+
+if __name__ == "__main__":
+    plt.plot(np.array(data)[:,0], np.array(data)[:,1], 'ro')
+    for tau in (0.01, 2, 100):
+        print("Loss for tau = " + str(tau) + ": " + str(compute_loss(tau)))
+        x = np.arange(0, 12, 0.1)
+        plt.plot(x, f_plot(x, tau), label="tau = " + str(tau))
+    plt.xlabel("x")
+    plt.ylabel("f(x)")
+    plt.legend()
+    plt.show()
